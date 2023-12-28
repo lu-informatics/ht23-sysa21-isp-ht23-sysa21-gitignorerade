6ea26efb2d7aebd8de63292af9c3980e392dbe15
Markussehlberg <ma0142se-s@student.lu.se>
2023-12-28 14:59:47 +0100

    commit

--- logisticssystem/src/main/java/lu/ics/se/Main.java
+++ logisticssystem/src/main/java/lu/ics/se/Main.java
@@ -1,31 +1,35 @@
 package lu.ics.se;
 
 import javafx.application.Application;
-
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
-
 
 public class Main extends Application {
 
     public static void main(String[] args) {
-        launch();
+        launch(args); // Pass command-line arguments to the application
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-       FXMLLoader loader = new FXMLLoader(getClass().getResource("MainViewVikingExpress.fxml"));
-        Parent root = loader.load();
+    public void start(Stage primaryStage) {
+        try {
+            // Load the FXML file. Adjust the path if your FXML file is in a different package
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/lu/ics/se/MainViewVikingExpress.fxml"));
+            Parent root = loader.load();
 
-        
-        
-        Scene scene = new Scene(root, 800, 600); // Set your preferred width and height
+            // Create the scene with the loaded root node and set the window size
+            Scene scene = new Scene(root, 800, 600); // Adjust the size as per your layout requirements
 
-        primaryStage.setTitle("Viking express");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+            // Set the title and scene, then show the primary stage
+            primaryStage.setTitle("Viking Express");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Handle exceptions here (e.g., show an alert to the user)
+        }
     }
 }
+
